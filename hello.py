f85d0ace6bd4b01e51f0b273eb43955e64fb1f5f
Uddipta Kashyap <150022560+uddiptakashyap@users.noreply.github.com>
2025-12-23 22:06:51 +0530

    changes

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8399502212:AAHLpoBqpDVQ3V2jTSNZMfQeSYqVUDMN314"
+BOT_TOKEN = "8539427459:AAHOj7azhNOCFWfL3QHDCyQ5SGhiw5ij2mk"
 
 players = set()
 completed = set()
@@ -197,3 +197,4 @@ app.add_handler(CommandHandler("end_task", end_task))
 app.add_handler(CommandHandler("reset", reset))
 
 app.run_polling()
+
